1eb647e11964b1d0dc695397aca64d3db479eca6
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-12-13 14:20:59 +0100

    Complete Exercise 13.2

---
+++ commands.sql
@@ -0,0 +1,21 @@
+CREATE TABLE blogs (
+  id SERIAL PRIMARY KEY,
+  author text,
+  url text NOT NULL,
+  title text NOT NULL,
+  likes integer DEFAULT 0
+);
+
+insert into
+  blogs (author, url, title)
+values
+  (
+    "Sebastian",
+    "https://sebastian.de",
+    "Sebastian's Blog"
+  );
+
+insert into
+  blogs (author, url, title)
+values
+  ("Peter", "https://peter.de", "Peter's Blog");
