fde8c926cc4e415b2d47ec48b156de6c5972e219
Sebastian Breit <sebastian.breitan+github@gmail.com>
2025-12-18 16:51:10 +0100

    Complete Exercise 13.6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -16,11 +16,29 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {
-  const amountDeleted = await Blog.destroy({ where: { id: req.params.id } });
-  if (amountDeleted === 0) {
+const blogFinder = async (req, res, next) => {
+  try {
+    req.blog = await Blog.findByPk(req.params.id);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+  if (!req.blog) {
     return res.status(404).end();
   }
+  next();
+};
+
+router.put("/:id", blogFinder, async (req, res) => {
+  if (req.body.likes === undefined) {
+    return res.status(400).json({ error: "Likes missing" });
+  }
+  req.blog.likes = req.body.likes;
+  req.blog.save();
+  return res.json({ likes: req.blog.likes });
+});
+
+router.delete("/:id", blogFinder, async (req, res) => {
+  req.blog.destroy();
   return res.status(204).end();
 });
 
